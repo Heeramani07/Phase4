551c2431d1dc446d7fa8ba079d9a086a586b418c
Heeramani07 <heeramanian07@gmail.com>
2022-09-29 12:12:50 +0530

    Portfolio

---
+++ MyPortfolio/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { TemplateComponent } from './template/template.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    SigninComponent,
+    SignupComponent,
+    TemplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
